e0241f62c81c1da0d0114816034976adb1ad51b9
nguyentrongbut <cloly031128@gmail.com>
2025-05-17 00:43:51 +0700

    init prj

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,15 @@
+import '@/app/ui/global.css';
+import React from "react";
+import {inter} from "@/app/ui/font";
+
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body>{children}</body>
+    <html lang="en" suppressHydrationWarning>
+      <body className={`${inter.className} antialiased`}>{children}</body>
     </html>
   );
 }
